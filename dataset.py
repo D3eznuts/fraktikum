b2fe86a0adc14a490cc2d664827e0bfbaaf8d39f
Mahardika-Ardi <arfurm198@gmail.com>
2026-05-04 11:27:14 +0700

    Refactor code structure for improved readability and maintainability

--- dataset.py
+++ dataset.py
@@ -3,4 +3,8 @@ import matplotlib as plt
 import seaborn as sns
 
 df = pd.read_csv('./dataset/Dragon_Ball_Data_Set.csv')
-print(df.head)
+print(df.head())
+print(df.info())
+print(df.isnull().sum())
+
+df = df[df['Power_Level'] > 0]
