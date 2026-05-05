ee74799ef09434dc0c9ad17358c97939a40451a0
D3eznuts <dean_pribadi_33rpl@student.smktelkom.mlg-sch.id>
2026-05-05 17:46:04 +0700

    Remove introductory comments from dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,3 @@
-# ================================
-# TUGAS SISWA 1–6 (VERSI ADAPTASI DATASET)
-# ================================
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
